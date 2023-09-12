4789b918a59f687cd31abccb13a0e7bd1a4fc248
sasikumarpy <144815035+sasikumarpy@users.noreply.github.com>
2023-09-12 07:29:36 -0700

    Add files via upload

---
+++ Mrak App1.py
@@ -0,0 +1,10 @@
+Name="sasikumar"
+plac="kumaran natu"
+Tamil=100
+English=100
+mas=100
+phy=100
+che=100
+computer=100
+Totel=Tamil+English+mas+phy+che+computer
+print("Thish is",Name,plac,"my Tamil mark",Tamil,"my English mark",English,"my mas mark",mas,"my phy mark",phy,"my che mark",che,"my computer mark",computer,"my Totel mark",Totel)
